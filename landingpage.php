6fa20c63a65edeafce7285f3fc822d9b6af34ac0
wilianbarahona <wilianbarahona10m@gmail.com>
2018-04-26 01:45:44 -0600

    nuevos frameworks

---
+++ landingpage.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aerolinea Sosa</title>
+
+    <link rel="icon" href="img/icon-page-html.png">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/landigPage.css" rel="stylesheet">
+  </head>
+
+  <body>
+  <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button style="background-color:#2C2C39" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span><i class="glyphicon glyphicon-align-justify"></i>
+                </button>
+               <a class="navbar-brand" href="register.php" style="margin-bottom: 20px;"><img src="img/freezer.png" class="img-responsive" alt="" width="150px"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <button onclick="location='register.php'" class="btn-registro">  
+                        Registrarse
+                    </button>
+                </li>
+                <li>
+                      <button onclick="location='login.php'" class="btn-conectar" >
+                            Conectarse
+                     </button>
+
+                </li>
+              </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+      
+
+            
+
+   
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="img/bg.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1 class="size-font-carrusel"><b>Tres meses de Premium por 0.99 USD.</b></h1>
+              <p>Solo Freezer te ofrece las mejores ofertas del mercado, Enterate de nuestra ofertas</p>
+              <p><a class="btn btn-lg btn-primary" href="offers.html" role="button">Mas informacion</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="img/features/manBlack.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1 class="size-font-carrusel"><b>Premium para toda tu familia, Por solo 8.99 USD.</b></h1>
+              <p>Puedes difrutar de Freezer con toda tu familia, sin restricciones.</p>
+              <p><a class="btn btn-lg btn-primary" href="offers.html" role="button">Mas informacion</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="img/features/greg.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+               <h1 class="size-font-carrusel"><b>Escucha tu musica en todas partes.</b></h1>
+              <p>Freezer es una plataforma que funciona en todos los dispositivos con una misma suscripcion premium+</p>
+              <p><a class="btn btn-lg btn-primary" href="devices.html" role="button">Mas informacion</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
+
+  
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="col-lg-12" style="text-align: center">
+                  <img class="img-circle" src="img/features/3.jpg" alt="Generic placeholder image" width="140" height="140">
+                  <h2>Disfruta de tus Artistas favoritos</h2>
+                  <p>Con Frezzer puedes disfrutar de tus artistas favoritos las 24 horas del dia, con musica de la mas alta calidad.</p>
+          </div>
+          
+        </div><!-- /.col-lg-6 -->
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-lg-12" style="text-align: center">
+                  <img class="img-circle" src="img/features/flow_iphone.png" alt="Generic placeholder image" width="140" height="140">
+                  <h2>Prueba nuestra aplicacion Movil</h2>
+                  <p>Para una mejor experiencia con Freezer prueba nuestra aplicacion movil, la puedes descargar de manera gratuita</p>
+                  <p><a class="btn btn-default" href="#" role="button">Ver detalles &raquo;</a></p>
+             </div>
+          
+        </div><!-- /.col-lg-6 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Una cuenta. Escucha tu música en todas partes</h2>
+          <p class="lead">Utiliza una sola cuenta en Freezer y disfruta de tu Flow en todos tus dispositivos</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/features/devices.png" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h1><b>¿Qué hay en Freezer?</b></h1>
+          <h2><b>Musica</b></h2>
+          <p class="lead">Freezer tiene millones de canciones. Escucha tu música favorita, descubre nuevas canciones y arma tu colección personal.</p>
+          <h2><b>Playlists</b></h2>
+          <p class="lead">Disfruta de playlists para cada estado de ánimo, armadas por fans y especialistas del mundo de la música. </p>
+          <h2><b>Nuevos lanzamientos</b></h2>
+          <p class="lead">Escucha los lanzamientos de sencillos y álbumes de la semana, y descubre qué está sonando en el top 50.</p>
+        </div>
+        <div class="col-md-4 col-md-pull-7" style="margin-top:35px">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" src="img/landingPage/album.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+
+        <!-- <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12s">
+            <div class="jumbotron">
+              <div class="container" style="margin-top:-33px">
+                <h2>Freezer FREE</h2>
+                <h2><b>0.00 USD/mes</b></h2>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12s">
+          <div class="jumbotron">
+              <div class="container"></div>
+            </div>
+        </div>
+      </div> -->
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        
+        <p>&copy; 2017 Company, Inc.</p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-3.2.1.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/holder.min.js"></script>
+    <script src="js/landingPage.min.js"></script>
+    
+  </body>
+</html>
