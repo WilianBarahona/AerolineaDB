9ca95f6b8ca29965d69d1a233605c32ad1f4d3de
wilianbarahona <wilianbarahona10m@gmail.com>
2018-04-25 21:14:06 -0600

    scrips db

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -1,6 +1,7 @@
 <div id="sidebar" class="col-sm-3 col-md-3 col-lg-2 sidebar">
         <ul class="nav nav-sidebar" id="dashboard">
 
+<!--<a href=""><img src="../img/icon-aerolinea.png" class="img-responsive" alt=""></a>-->
             <li id="btn-dashboard" class="active">
               <a href="#"><span class="glyphicon glyphicon-home"></span> Dashboard</a>
             </li>
@@ -12,19 +13,19 @@
             <li id="btn_tripulantes">
               <a href="#"><span class="glyphicon glyphicon-cd"></span> Tripulantes</a>
             </li>
-            <li>
+            <li id="btn-pasajeros">
               <a href="#"><span class="glyphicon glyphicon-music"></span> Pasajeros</a>
             </li>
 
-            <li>
+            <li id="btn-vuelos">
               <a href="#"><span class="glyphicon glyphicon-list"></span> Vuelos</a>
             </li>
 
-            <li>
+            <li id="btn-">
               <a href="#"><span class="glyphicon glyphicon-text-width"></span> Idiomas</a>
             </li>
 
-            <li>
+            <li id="btn-">
                 <a href="#"><span class="glyphicon glyphicon-screenshot"></span>Paises</a>
             </li>
         </ul>
