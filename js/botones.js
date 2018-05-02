eb80830a4e771777d146c45e1d87f16a078bdd76
WilianBarahona <wilianbarahona10m@gmail.com>
2018-05-02 05:32:07 -0600

    reservaciones html

---
+++ js/botones.js
@@ -0,0 +1,241 @@
+
+$("#btn-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+  $("#txt-asiento").val('A1');
+});
+$("#btn1-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn2-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn3-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn4-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn5-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn6-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn7-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn8-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn9-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn10-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn11-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn12-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn13-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn14-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn15-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn16-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn17-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn18-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+
+//fila #2
+$("#btn19-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn20-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn21-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn22-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn23-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn24-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn25-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn26-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn27-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn28-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn29-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn30-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn31-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn32-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn33-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn34-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn35-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn36-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+
+
+//fila #3
+
+$("#btn37-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn38-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn39-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn40-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn41-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn42-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn43-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn44-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn45-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn46-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn47-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn48-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn49-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn50-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn51-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn52-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn53-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn54-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn55-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn56-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+  //Otra fila
+
+$("#btn57-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn58-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn59-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn60-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn61-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn62-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn63-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn64-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn65-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn66-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn67-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn68-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn69-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn70-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn71-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn72-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn73-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn74-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn75-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
+$("#btn76-color").click(function(){
+  $(this).toggleClass("btn-primary btn-danger");
+});
